796a62945128168e9728dc280d0028d52a509178
maedilaziman <54487399+maedilaziman@users.noreply.github.com>
2022-09-02 20:36:11 +0700

    Create manual LoginModel
    Since test stop to track make the file not tracked even we make new file with the same name,
    so I create manual file

---
+++ TadaApp/model/LoginModel.swift
@@ -0,0 +1,55 @@
+//
+//  LoginModel.swift
+//  TadaApp
+//
+//  Created by maedi laziman on 29/01/22.
+//  Copyright © 2022 maedi laziman. All rights reserved.
+//
+
+import Foundation
+
+struct LoginModel {
+    var userName = ""
+    var password = ""
+    var cpass = ""
+    
+    init(userName: String, password: String) {
+        self.userName = userName
+        self.password = password
+    }
+    
+    var isZeroUserName: Bool {
+        if userName.count == 0 {
+            return true
+        }
+        return false
+    }
+    
+    var isValidUserName: Bool {
+        if userName.count < CharacterRange.userNameMinLength ||
+            userName.count > CharacterRange.userNameMaxLength {
+            return false
+        }
+        return true
+    }
+    
+    var isZeroPassword: Bool {
+        if password.count == 0 {
+            return true
+        }
+        return false
+    }
+    
+    var isValidPassword: Bool {
+        if password.count < CharacterRange.passwordMinLength ||
+            password.count > CharacterRange.passwordMaxLength {
+            return false
+        }
+        
+        return true
+    }
+    
+    var fullName: String? {
+        return "\(userName)"
+    }
+}
